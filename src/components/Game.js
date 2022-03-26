23c904d77062026e1e6fa307f1ac930796f17b65
Sharonfish <99145316+Sharonfish@users.noreply.github.com>
2022-03-26 13:27:25 -0700

    Update Game.js

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,15 +2,20 @@ import React from "react";
 import './styles/Game.css';
 import Board from "./Board";
 
-function Game() {
-    
-    return (
+class Game extends React.Component {
+    render() {
+      return (
         <div className="game">
-            <div className="game-board">
-                <Board />
-            </div>
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-    )
+      );
+    }
 }
 
 export default Game;
